f8a22ef5de120da12e9f5f16b3aa6b2f0b2d7335
Andy Hall <andy.hall@jaar.co.uk>
2018-12-19 12:25:09 +0000

    moved non-critical data fields to embedded schema

--- models/event.js
+++ models/event.js
@@ -8,20 +8,12 @@ const schema = new Schema(
     "timestamp": Number,
     "event": String,
     "smtp-id": String,
-    "useragent": String,
-    "ip": String,
     "sg_event_id": { type: String, unique: true },
     "sg_message_id": String,
     "reason": String,
     "status": String,
     "response": String,
-    "tls": String,
-    "url": String,
-    "attempt": Number,
-    "category": String,
-    "asm_group_id": Number,
-    "marketing_campaign_id": Number,
-    "marketing_campaign_name": String
+    "eventData": new Schema({}, {strict: false})
   },
   { collection: 'events' },
 );
