0feccda860f7fdd3d819ab8fd944d1ce0201f29e
Andy Hall <andy.hall@memiah.co.uk>
2024-05-10 15:18:20 +0100

    Update node version to 20

--- src/Handler.js
+++ src/Handler.js
@@ -1,6 +1,6 @@
-"use strict";
+'use strict';
 
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const defaults = require('./const/defaults');
 const AWS = require('aws-sdk');
 
@@ -12,144 +12,143 @@ let dbConnection = null;
 
 // lambda entry handler
 const handler = async (event, context) => {
-
-    let time;
-    const logTime = (msg) => {
-        if(msg) {
-            const diff = new Date().getTime() - time;
-            console.log(`${msg}: ${diff}ms`);
-        }
-        time = new Date().getTime();
+  let time;
+  const logTime = (msg) => {
+    if (msg) {
+      const diff = new Date().getTime() - time;
+      console.log(`${msg}: ${diff}ms`);
     }
+    time = new Date().getTime();
+  };
 
-    // if database connection not active, create it in global scope
-    if (dbConnection == null) {
-        logTime();
-        try {
-            dbConnection = await mongoose.createConnection(uri, {
-            bufferCommands: false,
-            bufferMaxEntries: 0,
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useUnifiedTopology: true
-            });
-        } 
-        catch(err) {
-            const response = { "statusCode": 500, "body": err }
-            return response;
-        }
-        // create model and attach to global database connection 
-        const eventSchema = require('./models/event');
-        dbConnection.model('Event', eventSchema);
-
-        logTime('DB Connection Setup');
-    }
-
-    //swap out event for test file when in dev
-    if(process.env.TESTFILE) {
-        event = require(process.env.TESTFILE);
-    }
-
-    const query = event.params.querystring;
-    const site_name = query.s || '';
-    event = event['body-json'];
-
-    const eventModel = dbConnection.model('Event'); 
-
-    // get event data from gateway passthrough
-    let eventData = event; 
-
-    console.log('Events received: ',eventData.length);
-
+  // if database connection not active, create it in global scope
+  if (dbConnection == null) {
     logTime();
-    // if event data is a string, parse it
-    if(typeof eventData == "string") {
-        eventData = JSON.parse(eventData);
-    }
-
-    // separate unpacked fields and put everything else in eventData embedded data
     try {
-        const handledFields = Object.keys(eventModel.schema.obj);
-        eventData = eventData.map(event => {
-            let mappedEvent = { 'site_name': site_name, 'info': {}, member: {} };
-            Object.keys(event).forEach(key => {
-            if (handledFields.indexOf(key) > -1) {
-                mappedEvent[key] = event[key];
-            } else {
-                mappedEvent.info[key] = event[key];
-            }
-            });
-            return mappedEvent;
-        });
-    }
-    catch(err) {
-       return { "statusCode": 500, "body": err }
+      dbConnection = await mongoose.createConnection(uri, {
+        bufferCommands: false,
+        bufferMaxEntries: 0,
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+      });
+    } catch (err) {
+      const response = { statusCode: 500, body: err };
+      return response;
     }
 
-    logTime('Prepare Data');
+    // create model and attach to global database connection
+    const eventSchema = require('./models/event');
+    dbConnection.model('Event', eventSchema);
 
-    // initialise return result
-    const result = { received: eventData.length, dupes: 0, errors: 0, affected: 0 };
+    logTime('DB Connection Setup');
+  }
 
-    // if category is array, replace with first element (sendgrid changed data format but we only want a string )
-    eventData = eventData.map(e => { return { ...e, category: Array.isArray(e.category) ? e.category[0] : e.category }});
+  //swap out event for test file when in dev
+  if (process.env.TESTFILE) {
+    event = require(process.env.TESTFILE);
+  }
 
-    logTime();
-    await new Promise(resolve => {
-        // insert records
-        eventModel.insertMany(eventData, { ordered: false }, (error, docs) => { 
-            // count dupes and non-dupe errors
-            result.affected = (docs && docs.length) || 0;
-            if(error) {
-                console.log('MongoDB Error',error);
-                if (error.writeErrors) {
-                    result.dupes = error.writeErrors.filter( error => error.err.code == 11000 ).length;
-                    result.errors = error.writeErrors.length - result.dupes;
-                }
-            }
-            resolve(true);
-        });
+  const query = event.params.querystring;
+  const site_name = query.s || '';
+  event = event['body-json'];
+
+  const eventModel = dbConnection.model('Event');
+
+  // get event data from gateway passthrough
+  let eventData = event;
+
+  console.log('Events received: ', eventData.length);
+
+  logTime();
+  // if event data is a string, parse it
+  if (typeof eventData == 'string') {
+    eventData = JSON.parse(eventData);
+  }
+
+  // separate unpacked fields and put everything else in eventData embedded data
+  try {
+    const handledFields = Object.keys(eventModel.schema.obj);
+    eventData = eventData.map((event) => {
+      let mappedEvent = { site_name: site_name, info: {}, member: {} };
+      Object.keys(event).forEach((key) => {
+        if (handledFields.indexOf(key) > -1) {
+          mappedEvent[key] = event[key];
+        } else {
+          mappedEvent.info[key] = event[key];
+        }
+      });
+      return mappedEvent;
     });
-    
-    logTime('Database Inserts', result);
+  } catch (err) {
+    return { statusCode: 500, body: err };
+  }
 
-    const statusCode = result.errors || !result.affected ? 500 : 200
+  logTime('Prepare Data');
 
-    var sqsMessageId = '';
-    if(statusCode == 500 && process.env.SQS_REDRIVE_QUEUE) {
-        const sqs = new AWS.SQS({ region: process.env.SQS_REDRIVE_QUEUE.split('.')[1] });
-        sqsMessageId = await new Promise(resolve => {
-            const params = {
-                QueueUrl: process.env.SQS_REDRIVE_QUEUE,
-                MessageBody: JSON.stringify(eventData),
-            }
-            sqs.sendMessage(params, (err, data) => {
-                if (err) {
-                    console.log('Erroring sending to sqs', err);
-                    console.log('EventData', eventData);
-                    resolve(false);
-                }
-                if (data) {
-                    resolve(data.MessageId);
-                }
-            });
-        });
-    }
+  // initialise return result
+  const result = { received: eventData.length, dupes: 0, errors: 0, affected: 0 };
 
-    if(result.errors > 0) {
-        console.error('errorsFoundInData', { sqsMessageId, result });
-    }
+  // if category is array, replace with first element (sendgrid changed data format but we only want a string )
+  eventData = eventData.map((e) => {
+    return { ...e, category: Array.isArray(e.category) ? e.category[0] : e.category };
+  });
 
-    if(result.dupes > 0) {
-        console.warn('dupesFoundInData', { sqsMessageId, result });
-    }
+  logTime();
+  await new Promise((resolve) => {
+    // insert records
+    eventModel.insertMany(eventData, { ordered: false }, (error, docs) => {
+      // count dupes and non-dupe errors
+      result.affected = (docs && docs.length) || 0;
+      if (error) {
+        console.log('MongoDB Error', error);
+        if (error.writeErrors) {
+          result.dupes = error.writeErrors.filter((error) => error.err.code == 11000).length;
+          result.errors = error.writeErrors.length - result.dupes;
+        }
+      }
+      resolve(true);
+    });
+  });
 
-    if(result.affected == 0) {
-        console.error('noAffectedRows', { sqsMessageId, result });
-    }
+  logTime('Database Inserts', result);
 
-    return { "statusCode": statusCode, "body": result };
-  
+  const statusCode = result.errors || !result.affected ? 500 : 200;
+
+  var sqsMessageId = '';
+  if (statusCode == 500 && process.env.SQS_REDRIVE_QUEUE) {
+    const sqs = new AWS.SQS({ region: process.env.SQS_REDRIVE_QUEUE.split('.')[1] });
+    sqsMessageId = await new Promise((resolve) => {
+      const params = {
+        QueueUrl: process.env.SQS_REDRIVE_QUEUE,
+        MessageBody: JSON.stringify(eventData),
+      };
+      sqs.sendMessage(params, (err, data) => {
+        if (err) {
+          console.log('Erroring sending to sqs', err);
+          console.log('EventData', eventData);
+          resolve(false);
+        }
+        if (data) {
+          resolve(data.MessageId);
+        }
+      });
+    });
+  }
+
+  if (result.errors > 0) {
+    console.error('errorsFoundInData', { sqsMessageId, result });
+  }
+
+  if (result.dupes > 0) {
+    console.warn('dupesFoundInData', { sqsMessageId, result });
+  }
+
+  if (result.affected == 0) {
+    console.error('noAffectedRows', { sqsMessageId, result });
+  }
+
+  return { statusCode: statusCode, body: result };
 };
 
 module.exports = handler;
--- src/models/event.js
+++ src/models/event.js
@@ -1,32 +1,32 @@
 const mongoose = require('mongoose');
 const defaults = require('../const/defaults');
 
-const collection = process.env.COLLECTION || defaults.MONGO_COLLECTION; 
+const collection = process.env.COLLECTION || defaults.MONGO_COLLECTION;
 
 const Schema = mongoose.Schema;
 
 const schema = new Schema(
   {
-    "email": String,
-    "timestamp": { type: Number, index: true},
-    "event": { type: String, index: true},
-    "smtp-id": String,
-    "sg_event_id": { type: String, unique: true },
-    "sg_message_id": String,
-    "reason": String,
-    "status": String,
-    "response": String,
-    "category": String,
-    "site_name": String,
-    "ip": String,
-    "info": new Schema({}, {strict: false}), 
-    "member": new Schema({
-      "id": { type: Number, default: 0, index: true },
-      "email_type": String,
-      "contacted": { type: Number, default: 0 },
-      "resolved": { type: Number, default: 0, index: true},
-      "resolved_timestamp": Number
-    })
+    email: String,
+    timestamp: { type: Number, index: true },
+    event: { type: String, index: true },
+    'smtp-id': String,
+    sg_event_id: { type: String, unique: true },
+    sg_message_id: String,
+    reason: String,
+    status: String,
+    response: String,
+    category: String,
+    site_name: String,
+    ip: String,
+    info: new Schema({}, { strict: false }),
+    member: new Schema({
+      id: { type: Number, default: 0, index: true },
+      email_type: String,
+      contacted: { type: Number, default: 0 },
+      resolved: { type: Number, default: 0, index: true },
+      resolved_timestamp: Number,
+    }),
   },
   { collection },
 );
