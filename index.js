9ed4fc07caf27e37ef36a5335533ad3c10625a4f
Andy Hall <andy@supergoteam.com>
2026-02-11 14:10:58 +0000

    Refactor index.js

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 "use strict";
 
-const handler = require('./src/Handler');
+const handler = require("./src/Handler");
 
-// lambda entry handler
 exports.handler = async (event, context) => {
-
     context.callbackWaitsForEmptyEventLoop = false;
 
     const result = await handler(event);
 
-    console.log('sending result', result);
-
-    if(result.statusCode !== 200) {
-        context.fail(JSON.stringify(result));
-    }
+    console.log("sending result", result);
 
     return result;
-
 };
